389f47a820e7f1c95adc230b8745418dd4dc3b6a
David McDonald <davmcdon@gmail.com>
2012-07-24 00:27:45 -0700

    hartl 8.16

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120724003037) do
+ActiveRecord::Schema.define(:version => 20120724072102) do
 
   create_table "urls", :force => true do |t|
     t.string   "link"
@@ -22,9 +22,12 @@ ActiveRecord::Schema.define(:version => 20120724003037) do
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
     t.string   "password"
+    t.string   "remember_token"
   end
+
+  add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
 
 end
